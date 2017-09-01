7548a0a8fc83e9f946f07e79efc93ec3dffcf605
gabrielluiscapelli <gabrielluiscapelli@gmail.com>
2017-09-01 20:39:01 +0200

    json ajax

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,90 +6,71 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+        
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+        <script src="{{ asset('/js/app.js') }}"></script>
+        <script src="{{ asset('/js/vue-resource.min.js') }}"></script>
+        <div id="main" class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                   <h1>Lista</h1>
+                   <ul class="list-group">
+                      
+                       <div class="list-group-item" v-for="item in list">
+                            @{{item.title}}
+                            <a href="@{{item.guid}}" title="">@{{item.link}}</a>
+                        </div>
+                       
+                   </ul>
+                    
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-sm-8">
+                   <h1>json</h1>
+                   <pre>
+                       @{{$data | json}}
+                   </pre>
+                    
                 </div>
             </div>
+          
         </div>
+      
+        <script>
+
+            //var urlUsers = 'https://randomuser.me/api/?results=10';
+            var urlUsers = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.20minutos.es%2Frss%2F';
+            new Vue({
+             
+            });
+            new Vue({
+              el: '#main',
+              created: function () {
+                  this.getUsers();
+              },
+              data: {
+                list: [],
+              },
+              methods: {
+                getUsers: function () {
+                    this.$http.get(urlUsers).then(function(response){
+                        this.list = response.body.items;
+                    });
+                }
+              }
+              
+            });
+        </script>
+
     </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('node_modules/vue-resource/dist/vue-resource.min.js', 'public/js');
